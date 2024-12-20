f284ced4be6e57bb7e4ec2aa5a71430633cd5f70
mac <mac@macui-MacBookAir.local>
2024-12-20 13:55:36 +0900

    pinning status

--- src/javas/main/wedul/JavaApplication.java
+++ src/javas/main/wedul/JavaApplication.java
@@ -5,10 +5,9 @@ import javas.main.VThreadRunner;
 public class JavaApplication {
 
     public static void main(String[] args) throws Exception {
-        VThreadRunner.run(() -> {
-            Wedul w = new Wedul();
-            w.start();
-        });
+        Wedul w = new Wedul();
+        VThreadRunner.run(w::start);
+        VThreadRunner.run(w::start);
     }
 
 
--- src/javas/main/wedul/Wedul.java
+++ src/javas/main/wedul/Wedul.java
@@ -2,7 +2,8 @@ package javas.main.wedul;
 
 public class Wedul {
 
-    public synchronized void start() {
+    public synchronized void start() throws InterruptedException {
         System.out.println("start");
+        Thread.sleep(1000);
     }
 }
